04f08541257407ee9a1c6ae1ce6cb2012ebc21a2
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-03-10 20:35:55 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,19 @@
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length = 2);{
+} if(cardOne === cardTwo){
+  alert("You found a match!");
+}  else {
+  alert("Sorry, try again.");
+}
+
+
+console.log("Up and running!");
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
